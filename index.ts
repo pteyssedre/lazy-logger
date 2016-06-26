9034f282ba73707a5df3dc5c723114878679e6f9
pteyssedre <p0l1c3$974zaza>
2016-06-26 14:10:02 -0400

    Fixing level selection

---
+++ index.ts
@@ -0,0 +1,106 @@
+import colors = require("colors");
+
+export module LazyLog {
+
+    export enum LogLevel {
+        VERBOSE = 0xFFF,
+        INFO = 1 << 0,
+        DEBUG = 1 << 1,
+        WARNING = 1 << 2,
+        ERROR = 1 << 3,
+        CRITICAL = 1 << 4
+    }
+
+    export class Logger {
+
+        private level: LogLevel;
+        private separator: string;
+
+        constructor(level?: LogLevel) {
+            if (isNaN(level)) {
+                this.level = LogLevel.VERBOSE;
+            }
+            else {
+                this.level = level;
+            }
+            this.separator = "[@]";
+        }
+
+        private formatLine(args: any[]): string {
+            let line = "";
+            let sep = this.separator.split("@");
+            let b = sep[0];
+            let a = sep[1];
+            for (var i = 0; i < args.length; i++) {
+                let str = "";
+                let item = args[i];
+                switch ((typeof item).toLowerCase()) {
+                    case "function":
+                        let tr = b + item;
+                        tr = tr.replace(/(\r\n|\n|\r)/gm, "");
+                        while (tr.indexOf("  ") > -1) {
+                            tr = tr.replace("  ", " ");
+                        }
+                        str += tr + a;
+                        break;
+                    case "object":
+                        str += b + JSON.stringify(item) + a;
+                        break;
+                    default:
+                        str += b + item + a;
+                        break;
+                }
+                line += " " + str;
+            }
+
+            return line.trim();
+        }
+
+        private log(level: LogLevel, any: any[]): void {
+            var data = [];
+            data.push(LogLevel[level]);
+            data.push(new Date().toISOString());
+            for (var i = 0; i < any.length; i++) {
+                data.push(any[i]);
+            }
+            let line = this.formatLine(data);
+            console.log(line);
+        }
+
+        public v(...any: any[]): void {
+            if (this.level & LogLevel.VERBOSE) {
+                this.log(LogLevel.VERBOSE, any);
+            }
+        }
+
+        public i(...any: any[]): void {
+            if (this.level & LogLevel.INFO) {
+                this.log(LogLevel.INFO, any);
+            }
+        }
+
+        public d(...any: any[]): void {
+            if (this.level & LogLevel.DEBUG) {
+                this.log(LogLevel.DEBUG, any);
+            }
+        }
+
+        public w(...any: any[]): void {
+            if (this.level & LogLevel.WARNING) {
+                this.log(LogLevel.WARNING, any);
+            }
+        }
+
+        public e(...any: any[]): void {
+            if (this.level & LogLevel.ERROR) {
+                this.log(LogLevel.ERROR, any);
+            }
+        }
+
+        public c(...any: any[]): void {
+            if (this.level & LogLevel.CRITICAL) {
+                this.log(LogLevel.CRITICAL, any);
+            }
+        }
+    }
+}
---
+++ test/test.js
@@ -0,0 +1,27 @@
+var chai = require("chai");
+var expect = chai.expect;
+var LazyLog = require("../dist/index").LazyLog;
+var LogLevel = LazyLog.LogLevel;
+var Logger = LazyLog.Logger;
+
+
+describe("Logger", function () {
+
+    var Log = new Logger(LogLevel.CRITICAL);
+
+    var data = function (){
+        var e = Math.floor(Math.random() + 1) * 7;
+    };
+    var o = {
+        name: "jena",
+        email : "toto@jena.com"
+    };
+    it("Should log", function () {
+        Log.v("verbose", "test", o, data);
+        Log.i("info", "test", o, data);
+        Log.d("debug", "test", o, data);
+        Log.w("warn", "test", o, data);
+        Log.e("error", "test", o, data);
+        Log.c("critical", "test", o, data);
+    })
+});
